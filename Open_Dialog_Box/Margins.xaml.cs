d2bf0f005230479b3e05e02509c37da49bf8abb4
Inma Gijón <igijoninf@gmail.com>
2023-10-08 17:07:33 +0200

    Abrir cuadros de diálogo

---
+++ Open_Dialog_Box/Margins.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Open_Dialog_Box
+{
+    /// <summary>
+    /// Lógica de interacción para Margins.xaml
+    /// </summary>
+    public partial class Margins : Window
+    {
+        public Margins()
+        {
+            InitializeComponent();
+        }
+    }
+}
